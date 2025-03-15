f6f7a788ab9c70e185eecbefb538a6867d4f50a4
PierceLe <haichau.hvsg@gmail.com>
2025-03-15 14:20:17 +1100

    feat: reset tu dau

--- backend/src/routes/v1/index.js
+++ backend/src/routes/v1/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const authRoute = require('./auth.route');
 const userRoute = require('./user.route');
 const docsRoute = require('./docs.route');
-const crisisRoute = require('./crisis.route'); // Import route Crisis
 const config = require('../../config/config');
 
 const router = express.Router();
@@ -15,10 +14,6 @@ const defaultRoutes = [
   {
     path: '/users',
     route: userRoute,
-  },
-  {
-    path: '/crises',
-    route: crisisRoute, // Thêm đường dẫn cho Crisis
   },
 ];
 
--- backend/src/validations/index.js
+++ backend/src/validations/index.js
@@ -1,4 +1,2 @@
 module.exports.authValidation = require('./auth.validation');
 module.exports.userValidation = require('./user.validation');
-module.exports.crisisValidation = require('./crisis.validation');
-module.exports.gameStateValidation = require('./gameState.validation');
