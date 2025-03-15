f704c2d40828c0f25281394ca40fee15e474df31
PierceLe <haichau.hvsg@gmail.com>
2025-03-15 21:51:17 +1100

    fix: update taskState schema

--- backend/src/models/taskState.model.js
+++ backend/src/models/taskState.model.js
@@ -1,12 +1,17 @@
 const mongoose = require('mongoose');
 const { toJSON, paginate } = require('./plugins');
 
-const taskStateSchema = mongoose.Schema({
-  task_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Task', required: true },
-  sp_left: { type: Number, required: true },
-  sp_predicted: { type: Number, required: true },
-  status: { type: String, enum: ['pending', 'in_progress', 'completed'], required: true },
-});
+const taskStateSchema = mongoose.Schema(
+  {
+    task_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Task', required: true },
+    sp_left: { type: Number, required: true },
+    sp_predicted: { type: Number, required: true },
+    status: { type: String, enum: ['pending', 'in_progress', 'testing', 'completed'], required: true },
+    assignee: { type: mongoose.Schema.Types.ObjectId, ref: 'NPC', required: true },
+    reviewer: { type: mongoose.Schema.Types.ObjectId, ref: 'NPC', required: true },
+  },
+  { timestamps: true } // Automatically adds createdAt and updatedAt fields
+);
 
 taskStateSchema.plugin(toJSON);
 taskStateSchema.plugin(paginate);
