e97069f6156297b3d7b14eedb56493e062d5bf33
PierceLe <haichau.hvsg@gmail.com>
2025-03-15 15:04:58 +1100

    feat: init db with hotfix

--- backend/src/models/crisis.model.js
+++ backend/src/models/crisis.model.js
@@ -5,6 +5,7 @@ const crisisSchema = mongoose.Schema({
   description: { type: String, required: true },
   role_needed: { type: String, required: true },
   effect: [{ type: Number }],
+  resolution: { type: String, required: true },
 });
 
 const Crisis = mongoose.model('Crisis', crisisSchema);
